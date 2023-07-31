0f75efbdbb0fc1289956d1d0c397b59e130d29de
Хлыбов А.Г <AGKhlybov@sberbank.ru>
2023-07-31 17:14:11 +0300

    Add new wersion

--- telbot.py
+++ telbot.py
@@ -25,3 +25,44 @@ class TelBot:
 if __name__ == '__main__':
     bot = TelBot()
     bot.run_bot()
+
+from flask import Flask, request
+import telepot
+import urllib3
+
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+secret = "234h1l234hkasdfj;kladjsfkjioq4jklj5;k"
+bot = telepot.Bot('6622677042:AAHrFrwMPjsok4UzK86ByMgN3B8mAnpLqZ0')
+bot.setWebhook("https://halexx7.pythonanywhere.com/{}".format(secret), max_connections=1)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(secret), methods=["POST"])
+def telegram_webhook():
+    update = request.get_json()
+    
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    time.sleep(1)
+    bot.set_webhook(url=)
+
+
+# secret = ""
+# TOKEN = ''
+# bot = telebot.TeleBot(TOKEN, threaded=False)
+# bot.remove_webhook()
+# bot.set_webhook(url="https://example.pythonanywhere.com/{}".format(secret))
